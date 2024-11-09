e7686cfbadaac770e3b8bd101d63a31ba31dd327
iftikhar <iftikhar.uddin.fazal@gmail.com>
2024-11-09 19:50:54 +0300

    Collections with @Value

--- springcore/src/main/java/com/springcore/stereotype/Student.java
+++ springcore/src/main/java/com/springcore/stereotype/Student.java
@@ -1,16 +1,20 @@
 package com.springcore.stereotype;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Student {
 
-	@Value("Syed Iftikhar")
+    @Value("Syed Iftikhar")
     private String name;
-	
-	@Value("Doha, Qatar")
+    
+    @Value("Doha, Qatar")
     private String city;
+    
+    @Value("#{addressList}")  // Injecting the list using Spring Expression Language (SpEL)
+    private List<String> address;
 
     // No-argument constructor
     public Student() {}
@@ -35,12 +39,23 @@ public class Student {
         this.city = city;
     }
 
+    // Getter for address
+    public List<String> getAddress() {
+        return address;
+    }
+
+    // Setter for address
+    public void setAddress(List<String> address) {
+        this.address = address;
+    }
+
     // Optional: Override toString() for easy display
     @Override
     public String toString() {
         return "Student{" +
                 "name='" + name + '\'' +
                 ", city='" + city + '\'' +
+                ", address=" + address +
                 '}';
     }
 }
