62e24203b2741a4ebd83a05039dc6db48043446d
iftikhar <iftikhar.uddin.fazal@gmail.com>
2024-11-09 06:36:11 +0300

    @Qualifier Annotation with Autowiring

--- springcore/src/main/java/com/springcore/autowire/annotation/Employee.java
+++ springcore/src/main/java/com/springcore/autowire/annotation/Employee.java
@@ -1,9 +1,12 @@
 package com.springcore.autowire.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Employee {
     
+	@Autowired
+	@Qualifier("address1")
     private Address address;
 
     // No-argument constructor
@@ -13,7 +16,7 @@ public class Employee {
 
     // Constructor with Address parameter
     
-    @Autowired
+    
     public Employee(Address address) {
         this.address = address;
     }
