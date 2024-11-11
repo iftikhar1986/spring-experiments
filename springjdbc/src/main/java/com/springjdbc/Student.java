d8aae8a8e2848fa5250eb870f11f2dc59600d452
iftikhar <iftikhar.uddin.fazal@gmail.com>
2024-11-11 22:44:45 +0300

    Database setup | Configuring Jdbc Template to execute query

---
+++ springjdbc/src/main/java/com/springjdbc/Student.java
@@ -0,0 +1,58 @@
+package com.springjdbc;
+
+public class Student {
+
+    private int id;
+    private String name;
+    private String city;
+
+    // Default constructor
+    public Student() {}
+
+    // Parameterized constructor
+    public Student(int id, String name, String city) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+    }
+
+    // Getter for id
+    public int getId() {
+        return id;
+    }
+
+    // Setter for id
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter for city
+    public String getCity() {
+        return city;
+    }
+
+    // Setter for city
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    // Override toString() for easy display
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
